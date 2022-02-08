e81fa0d6abee2c8d2ece43e47a81267b447ff54a
emil-mattsson-nti-johanneberg <emil.mattsson@elev.ga.ntig.se>
2022-02-08 15:34:43 +0100

    08/02-2022

--- projekt/app.rb
+++ projekt/app.rb
@@ -8,3 +8,12 @@ require 'bcrypt'
 get('/') do
     slim(:start)
 end
+
+get('/nyheter') do
+    db = SQLite3::Database.new("db/chinook-crud.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM albums")
+    p result 
+    slim(:"albums/index",locals:{nyheter:result})
+end
+
