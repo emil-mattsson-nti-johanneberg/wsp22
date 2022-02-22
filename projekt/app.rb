67cf3bc0fd305b117cc7eecab68977ad546f806e
emil-mattsson-nti-johanneberg <emil.mattsson@elev.ga.ntig.se>
2022-02-22 15:31:17 +0100

    22/05-2022

--- projekt/app.rb
+++ projekt/app.rb
@@ -17,3 +17,46 @@ get('/nyheter') do
     slim(:"albums/index",locals:{nyheter:result})
 end
 
+get('/showlogin') do
+    slim(:login)
+end
+
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+  
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
+      redirect('/documents')
+    else
+      "FEL LÖSENORD!"
+    end
+end
+
+
+get('/documents') do
+end
+
+
+
+post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+  
+    if (password == password_confirm)
+      #lägg till användare
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('db/todo2021-db')
+      db.execute('INSERT INTO users (username,pwdigest) VALUES (?,?)',username,password_digest)
+      redirect('/')
+    else
+      #felhantering
+      "lösenordet är fel"
+    end
+end
