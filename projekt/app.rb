c38012e0c0b1fc2fe63b88c314710551960e21cc
emil-mattsson-nti-johanneberg <emil.mattsson@elev.ga.ntig.se>
2022-03-15 15:32:25 +0100

    15/03-2022

--- projekt/app.rb
+++ projekt/app.rb
@@ -3,8 +3,6 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
-
-
 get('/') do
     slim(:start)
 end
@@ -14,15 +12,22 @@ get('/nyheter') do
     db.results_as_hash = true
     result = db.execute("SELECT * FROM albums")
     p result 
-    slim(:"albums/index",locals:{nyheter:result})
+    slim(:"nyheter/index",locals:{nyheter:result})
 end
 
 get('/kontakt') do
+    db = SQLite3::Database.new("db/kontakter.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM employees")
     slim(:contact)
 end
 
 get('/showlogin') do
     slim(:login)
+end
+
+get('/calender') do
+    slim(:calender)
 end
 
 get('/profile') do
@@ -54,9 +59,7 @@ post('/login') do
 end
 
 
-get('/documents') do
-    slim(:documents)
-end
+
 
 
 
@@ -68,7 +71,7 @@ post('/users/new') do
     if (password == password_confirm)
       #lägg till användare
       password_digest = BCrypt::Password.create(password)
-      db = SQLite3::Database.new('db/todo2021-db')
+      db = SQLite3::Database.new('')
       db.execute('INSERT INTO users (username,pwdigest) VALUES (?,?)',username,password_digest)
       redirect('/')
     else
@@ -77,6 +80,22 @@ post('/users/new') do
     end
 end
 
+get('/documenttingz') do
+  db = SQLite3::Database.new("db/kontakter.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM documents")
+  slim(:"documenttingz/index",locals:{documents:result})
+end
+
 get('/documenttingz/new') do
   slim(:"documenttingz/new")
+end
+
+post('/documenttingz/new') do
+  docTitle = params[:docTitle]
+  docLink = params[:docLink]
+  p "Vi fick in datan #{docTitle} och #{docLink}"
+  db = SQLite3::Database.new("db/kontakter.db")
+  db.execute("INSERT INTO documents (docTitle, docLink) VALUES (?,?)", docTitle, docLink)
+  redirect('/documenttingz')
 end
