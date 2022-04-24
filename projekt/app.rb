a46b72899a9bd7352b682dd088b80215aa2656f2
emil-mattsson-nti-johanneberg <emil.mattsson@elev.ga.ntig.se>
2022-04-24 23:04:39 +0200

    24/04

--- projekt/app.rb
+++ projekt/app.rb
@@ -20,7 +20,7 @@ get('/lag') do
   db = SQLite3::Database.new("db/kontakter.db")
   db.results_as_hash = true
   result = db.execute("SELECT * FROM teams")
-  slim(:"lag",locals:{teams:result})
+  slim(:"lag/index",locals:{albums:result})
 end
 
 get('/kontakt') do
@@ -38,7 +38,6 @@ get('/calender') do
     slim(:calender)
 end
 
-
 get('/register') do
   slim(:register)
 end
@@ -53,7 +52,7 @@ post('/login') do
       redirect('/error')
     end
     pwdigest = result["password_digest"]
-    id = result["Id"]
+    id = result["userID"]
   
     if BCrypt::Password.new(pwdigest) == password
       session[:id] = id
@@ -73,7 +72,8 @@ post('/users/new') do
       password_digest = BCrypt::Password.create(password)
       db = SQLite3::Database.new('db/kontakter.db')
       db.execute('INSERT INTO users (username,password_digest) VALUES (?,?)',username,password_digest)
-      redirect('/login')
+      db.execute('INSERT INTO userinfo (username) VALUES (?)',username)
+      redirect('/showlogin')
     else
       #felhantering
       "lösenordet är fel"
@@ -112,6 +112,26 @@ end
 get('/profil') do
   db = SQLite3::Database.new("db/kontakter.db")
   db.results_as_hash = true
-  result = db.execute("SELECT username FROM users WHERE Id= ?",session[:id]).first
-  slim(:"profil/index",locals:{users:result})
+  #result = db.execute("SELECT username FROM users WHERE userID= ?",session[:id]).first
+  result = db.execute("SELECT * FROM userinfo WHERE userID= ?",session[:id]).first
+  slim(:"profil/index",locals:{userinfo:result})
 end
+
+get('/profil/edit') do
+  db = SQLite3::Database.new("db/kontakter.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM userinfo WHERE userID= ?",session[:id]).first
+  slim(:"profil/edit",locals:{userinfo:result})
+end
+
+post('/profil/edit') do
+  fullname = params[:fullname]
+  age = params[:age]
+  team = params[:team]
+  db = SQLite3::Database.new("db/kontakter.db")
+  db.results_as_hash = true
+  #db.execute("INSERT INTO userinfo (fullname, age, team) VALUES (?,?,?)", fullname, age, team)
+  db.execute("UPDATE userinfo SET fullname=?,age=?,team=? WHERE userID = ?",fullname,age,team,session[:id])
+  redirect('/profil')
+end
+
